7958ef721fedc1f863743b9508e4b7212e1e2fee
Titanoboba <nik20052006@mail.ru>
2024-05-02 08:42:22 -0400

    modified comments in file hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,9 +4,10 @@ int main()
 {
         std::string name;
 
-        // Name input
+        // Input name
         std::cout << "Input name: ";
         std::cin >> name;
 
+        // Console output
         std::cout << "Hello world from " + name << std::endl;
 }
